cda47e2595e0311b6572bb771427ac03b32870f8
alexander784 <alexandernyaga1999@gmail.com>
2024-12-01 08:13:37 +0300

    Create registerview

--- auth/ruhusa/views.py
+++ auth/ruhusa/views.py
@@ -29,6 +29,36 @@ def login_page(request):
             return redirect('home/')
         
     return render(request, 'login.html')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.info(request,"Username already exists")
+            return redirect('/register/')
+        
+        user = User.objects.create_user(
+            first_name = first_name,
+            last_name = last_name,
+            username=username
+        )
+
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "Account created Successfully!")
+
+        return redirect('/register/')
+    return render(request, 'register.html')
+
+
     
         
         
